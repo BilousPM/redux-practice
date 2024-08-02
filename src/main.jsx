5fa1a5e214cea2e5aa17d4fd48ae1855c5d3f199
BilousPM <bilouspm@gmail.com>
2024-08-02 14:42:12 -0600

    configure Redux, create reducer, connect  to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import App from './App.jsx';
 import './index.css';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider>
+    <App />
+  </Provider>,
+);
---
+++ src/redux/counter/reduser.js
@@ -0,0 +1,16 @@
+const initialState = {
+  counter: 0,
+  step: 1,
+};
+
+export const counterRredcer = (state = initialState; action) => {
+  switch (action.type) {
+    case value:
+      
+      break;
+  
+    default:
+      return state
+      break;
+  }
+};
